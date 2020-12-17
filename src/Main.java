5c2c823a2c41d416b793cdc9cc1d3e7326bb3433
illuhayacino <56147785+illuhayacino@users.noreply.github.com>
2020-12-17 18:12:35 +0300

    Вывод объектов в консоль

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) {
+        HashMap<Integer,String> flatOwner = new HashMap<>();
+        flatOwner.put(11,"Viktor");
+        flatOwner.put(12,"Max");
+        flatOwner.put(13,"Maria");
+        flatOwner.put(14,"Felix");
+        flatOwner.put(15,"Hektor");
+        System.out.println(flatOwner);
+    }
+
+
+}
