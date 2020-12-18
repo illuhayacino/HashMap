84fa2ad3b6c2d884ddf9dd0c30768304fa6f5684
illuhayacino <56147785+illuhayacino@users.noreply.github.com>
2020-12-18 10:31:38 +0300

    добавление HashSet

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.HashSet;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,6 +9,13 @@ public class Main {
         flatOwner.put(13,"Maria");
         flatOwner.put(14,"Felix");
         flatOwner.put(15,"Hektor");
+
+        HashSet<Integer> keys = new HashSet<>();
+        keys.add(13);
+        keys.add(13);
+
+        System.out.println(keys);
+
         System.out.println(flatOwner);
     }
 
